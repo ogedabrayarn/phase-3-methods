fdacbc1e661996839afecfb369dbdbb7f4c208a5
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-19 00:35:08 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+ def greet_programmer
+    puts "Hello, programmer!"
+ end
+ greet_programmer
+
+ def greet(name)
+
+    if name == "Naureen"
+        puts "Hello, #{name}!"
+  elsif name == "Jimmy"
+    puts "Hello, #{name}!"
+  else
+    puts "Invalid"
+  end
+ end
+ greet("Naureen")
+ greet("Jimmy")
+
+def greet_with_default (name = "programmer")
+puts ("Hello, #{name}!")
+end
+
+def add(num1, num2)
+    num1 + num2
+end
+
+def halve(number)
+    if number.class != Integer
+       return nil
+    end
+        number / 2
+end
